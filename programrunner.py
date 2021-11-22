a00344d7fcf54f1b39183f86598c74d4fb54338f
madmax3000 <johnjose223@gmail.com>
2021-11-21 17:43:51 -0800

    update for i value

--- programrunner.py
+++ programrunner.py
@@ -3,8 +3,8 @@ import os
 import shutil
 benchmark_directory = "/home/johnj/gem5/m5out/benchmarks/458.sjeng"  #location of bench mark directory
 gem5_directory = '/home/johnj/gem5'    # location of the gem5  directory
-initial_count = 15
-final_count = 20
+initial_count = 3
+final_count = 5
 instruction_count = "1000"
 benchmark_location = benchmark_directory + "/src/benchmark"
 argument_location = benchmark_directory + "/data/test.txt"
@@ -32,9 +32,9 @@ with open(combination_data, 'r') as file:
         command_part3 = "--l1d_size="+ l1_data_size + " --l1i_size=" + l1_inst_size + " --l2_size=" + l2_data_size
         command_part4 = " --l1d_assoc="+ l1_data_aso_str + " --l1i_assoc=" + l1_ist_aso_str + " --l2_assoc=" + l2_data_aso_str + " --cacheline_size=" + l2_data_block_str
         #print(command_part1+command_part2+command_part3+command_part4)
-        os.system(command_part1+command_part2+command_part3+command_part4)
+        #os.system(command_part1+command_part2+command_part3+command_part4)
         src_stats_path = benchmark_directory + "/m5out/stats.txt"
-        dst_stats_path = benchmark_directory + "/cache_outputs/" + "/stats_" + str(i) + ".txt"
+        dst_stats_path = benchmark_directory + "/cache_outputs/" + "/stats_" + str(i+1) + ".txt"
         shutil.copy(src_stats_path, dst_stats_path)
         print('Copied stats file with default values ' + str(i) + '   iteration')
 
